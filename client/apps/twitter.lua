a6e5331e31fcaecdac5986174b3eabfd07d07e3a
Alzar <srichardson97402@hotmail.com>
2019-11-06 19:47:47 -0800

    Why the fuck does this break this callback? FIVEM PLEASE

--- client/apps/twitter.lua
+++ client/apps/twitter.lua
@@ -16,6 +16,6 @@ AddEventHandler('mythic_phone:client:MentionedInTweet', function(author)
     exports['mythic_notify']:SendAlert('inform', author .. ' Mentioned You In A Tweet', 2500, { ['background-color'] = '#039be5'  })
 end)
 
-RegisterNUICallback( 'NewTweet', function( data, cb )
-    Callbacks:ServerCallback('mythic_phone:server:NewTweet', data, cb)
+RegisterNUICallback('NewTweet', function(data, cb)
+    Callbacks:ServerCallback('mythic_phone:server:NewTweet', { message = data.message, mentions = data.mentions, hashtags = data.hashtags }, cb)
 end)
